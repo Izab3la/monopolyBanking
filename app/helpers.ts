d70b4de781477aad55beb9234e71c9875525eec0
nikodem <nikodemmarek11@gmail.com>
2024-04-06 11:24:05 +0000

    feat: CustomTransaction component

---
+++ app/helpers.ts
@@ -0,0 +1,23 @@
+import { PlayerI } from "./types";
+import { useSelector } from "react-redux";
+import { store, transfer as storeTransfer } from "./GameReducer";
+
+export function getPlayers(filter: (player: PlayerI) => boolean = () => true) {
+  return useSelector(({ players }: { players: PlayerI[] }) => players).filter(
+    filter,
+  );
+}
+
+export function transfer(from: string, to: string, amount: number) {
+  try {
+    store.dispatch(
+      storeTransfer({
+        from,
+        to,
+        amount,
+      }),
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
--- app/pages/Player.tsx
+++ app/pages/Player.tsx
@@ -3,22 +3,12 @@ import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import {
-  Button,
-  Card,
-  Icon,
-  IconButton,
-  Modal,
-  Portal,
-  Text,
-  TextInput,
-} from "react-native-paper";
-import { useDispatch, useSelector } from "react-redux";
+import { Text } from "react-native-paper";
 import { BackHandler } from "react-native";
 
-import { transfer } from "../GameReducer.ts";
+import { getPlayers } from "../helpers.ts";
 import { PlayerI } from "../components/Players.tsx";
-import PlayersList from "../components/PlayersList.tsx";
+import CustomTransaction from "../components/CustomTransaction.tsx";
 
 export default function Player({
   navigation,
@@ -40,28 +30,9 @@ export default function Player({
   }, []);
 
   const { name } = route.params;
-  const player = useSelector(
-    ({ players }: { players: PlayerI[] }) => players,
-  ).find((player: PlayerI) => player.name === name);
-  const dispatch = useDispatch();
-
-  const [visible, setVisible] = useState(false);
-  const [to, setTo] = useState("");
-  const [amount, setAmount] = useState("");
-
-  function sendTransfer() {
-    try {
-      dispatch(
-        transfer({
-          from: player.name,
-          to,
-          amount: Number(amount),
-        }),
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  const player = getPlayers().find(
+    (player: PlayerI) => player.name === name,
+  ) as PlayerI | undefined;
 
   return (
     <SafeAreaView
@@ -70,27 +41,6 @@ export default function Player({
         padding: 10,
       }}
     >
-      <Portal>
-        <Modal
-          visible={visible}
-          onDismiss={() => setVisible(0)}
-          contentContainerStyle={{
-            flex: 1,
-            backgroundColor: "white",
-            padding: 20,
-            margin: 20,
-          }}
-        >
-          <PlayersList
-            onPlayerPress={(name: string) => {
-              setTo(name);
-              setVisible(false);
-            }}
-            exclude={[player.name]}
-          />
-        </Modal>
-      </Portal>
-
       <View
         style={{
           justifyContent: "center",
@@ -105,50 +55,12 @@ export default function Player({
             color: "gray",
           }}
         >
-          {player.name}
+          {player?.name}
         </Text>
-        <Text variant="displayLarge">${player.balance}</Text>
+        <Text variant="displayLarge">${player?.balance}</Text>
       </View>
 
-      <Card style={{ marginHorizontal: 10 }}>
-        <Card.Content
-          style={{
-            gap: 10,
-          }}
-        >
-          <View
-            style={{
-              flexDirection: "row",
-            }}
-          >
-            <TextInput
-              style={{
-                flex: 1,
-              }}
-              mode="outlined"
-              label="Ammount"
-              inputMode="numeric"
-              value={amount}
-              onChangeText={setAmount}
-            />
-            <IconButton icon="arrow-up" onPress={sendTransfer} size={40} />
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
-            }}
-          >
-            <Icon source="arrow-right" size={40} />
-            <Button
-              style={{ flex: 1 }}
-              mode="outlined"
-              onPress={() => setVisible(true)}
-            >
-              {to || "To"}
-            </Button>
-          </View>
-        </Card.Content>
-      </Card>
+      {player?.name && <CustomTransaction from={player?.name} />}
 
       <StatusBar style="auto" />
     </SafeAreaView>
